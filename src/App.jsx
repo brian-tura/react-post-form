511905fcb12cbc261da524b71395da9cc3df0662
brian-tura <b.tura2002@gmail.com>
2025-05-14 17:00:44 +0200

    [UPD] Added axios call and hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,39 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  
-  
+
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false
+  })
+
+  const handleChange = (e) => {
+    const {name, value, type, checked} = e.target;
+    setFormData({
+      ...formData,
+      [name]: type ==='checkbox'
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts", formData).then((res) => {
+      console.log(res.data);
+      setFormData({
+        author: "",
+        title: "",
+        body: "",
+        public: false
+      })
+    })
+  }
+
 
   return (
     <>
-      
+
     </>
   )
 }
